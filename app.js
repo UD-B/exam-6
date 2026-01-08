c1666df9db688b9ed7bc21250bad1f579d9e1bdd
UD <yudibarak770@gmail.com>
2026-01-08 12:49:18 +0200

    final push

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ import { RegularPassenger } from "./classes/RegularPassenger.js";
 import { StudentPassenger } from "./classes/Student.js";
 
 const airport = new Airport("UD", "ud inc.", "1", 10, "BRK", "brk inc.", "2", 10, "GR", "gr inc.", "3", 10)
-const student = new StudentPassenger("yudi", 750, "kodcode")
+const student = new StudentPassenger("yudi", 1000, "kodcode")
 const regular = new RegularPassenger("barak", 850, "web developer", true)
 
 
 airport.flight1.buyTicket(student, 'vip')
 airport.flight1.buyTicket(student, 'regular')
-
